c09430331fc5c679b78bd7a016ebc3d7454c2754
Emma Wenzel <emmawenzel512@gmail.com>
2020-06-17 21:09:44 +0000

    Add test for 2 events in reverse order with just enough time between them

--- walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
+++ walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
@@ -452,8 +452,8 @@ public final class FindMeetingQueryTest {
 
   @Test
   public void fourNestedEvents() {
-    // Have an event for each person, but have one person's event fully contain another's event. We
-    // should see two options.
+    // Have an event for each person, but have one person's event fully contain another's event. 
+    // The events are also out of order. We should see three options.
     //
     // Events  :     |--A--|
     //                           |-B-|
@@ -480,6 +480,31 @@ public final class FindMeetingQueryTest {
         Arrays.asList(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TIME_0800AM, false), 
             TimeRange.fromStartEnd(TIME_0900AM, TIME_1000AM, false),
             TimeRange.fromStartEnd(TIME_1100AM, TimeRange.END_OF_DAY, true));
+
+    Assert.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void outOfOrderJustEnoughTime() {
+    // Two events in reverse chronological order with a possible time between them 
+    //
+    // Events  :              |--A---------|
+    //           |----B--|
+    // Day     : |-------------------------|
+    // Options :         |----| 
+
+    Collection<Event> events = Arrays.asList(
+        new Event("Event 2", TimeRange.fromStartDuration(TIME_1000AM, TimeRange.END_OF_DAY),
+            Arrays.asList(PERSON_B)),
+        new Event("Event 1", TimeRange.fromStartDuration(TimeRange.START_OF_DAY, TIME_0900AM),
+            Arrays.asList(PERSON_A)));
+
+    MeetingRequest request =
+        new MeetingRequest(Arrays.asList(PERSON_A, PERSON_B), DURATION_60_MINUTES);
+
+    Collection<TimeRange> actual = query.query(events, request);
+    Collection<TimeRange> expected =
+        Arrays.asList(TimeRange.fromStartEnd(TIME_0900AM, TIME_1000AM, false));
 
     Assert.assertEquals(expected, actual);
   }
